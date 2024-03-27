06cf7a2221007ac0a457caa1aff3a2632d2977d6
ABHIJITH SURESH <abhisuresh2020@gmail.com>
2024-03-27 17:31:16 +0530

    Learn

---
+++ Learning/Learn1.java
@@ -0,0 +1,27 @@
+package Learning;
+
+public class Learn1 {
+    public static void main(String[] args) {
+        int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int b[] = new int[10];
+        for (int i = 0; i < 10; i++) {
+            b[i] = a[i];
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println(b[i]);
+        }
+        int[][] c = new int[3][3];
+        int[][] d = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                c[i][j] = d[i][j];
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println(c[i][j]);
+            }
+        }
+        System.out.println("Hello World");
+    }
+}
